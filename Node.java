ee9bd498d07c1ec8928a5776d2ee964b9333485c
Giselle Chaves <112208391+GiselleChaves@users.noreply.github.com>
2023-10-28 20:25:09 -0300

    Creating SwitchCase in the App

---
+++ Node.java
@@ -0,0 +1,10 @@
+public class Node {
+    public int element;
+    public Node next, prev;
+
+    public Node(int element){
+      this.element = element;
+      this.next = null;
+      this.prev = null;
+    }
+  }
